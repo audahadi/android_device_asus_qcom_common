f3c58dc4b09a362996c967bacc182dde3c8e788e
jrior001 <jriordan001@gmail.com>
2015-07-18 22:41:52 -0400

    msm8916-common: Add custom keyhandler class for gestures
    * imported from oppo-common
    * initial support for TapToWake for zenfone2
    * more to follow for other gestures
    
    Change-Id: Idc0667e38f408d689950ad51ff2eac39394c4760

--- msm8916.mk
+++ msm8916.mk
@@ -95,6 +95,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
 
+# Keyhandler
+PRODUCT_PACKAGES += \
+    com.cyanogenmod.keyhandler
+
 # Keystore
 PRODUCT_PACKAGES += \
     keystore.msm8916
