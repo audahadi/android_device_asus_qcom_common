1540be7ee245225a8491b4dc1965820507fd579c
luca020400 <luca.stefani.ge1@gmail.com>
2015-12-23 16:56:37 +0100

    msm8916-common: Add heap and hwui inherits

--- msm8916.mk
+++ msm8916.mk
@@ -43,6 +43,9 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
+$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
+
 # Audio
 PRODUCT_PACKAGES += \
     audiod \
