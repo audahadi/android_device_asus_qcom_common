8c188686cdb5dd6aa715b25a7ac30b6168b5763e
Luca Stefani <luca.stefani.ge1@gmail.com>
2016-01-24 12:40:01 +0100

    msm8916-common: Import CAF liblights
    Change-Id: I1baa08dd757a5a2607fa7635c67d955858d5759d

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -113,6 +113,9 @@ TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
 # Keymaster
 TARGET_KEYMASTER_WAIT_FOR_QSEE := true
 
+# Lights
+TARGET_PROVIDES_LIBLIGHT := true
+
 # Malloc
 MALLOC_IMPL := dlmalloc
 
