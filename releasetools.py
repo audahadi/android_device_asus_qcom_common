be2d0ef2a4df3eab97751d3936c2839a1413da15
LuK1337 <priv.luk@gmail.com>
2016-01-31 01:00:11 +0100

    msm8916-common: Fix ditlib assert
    Change-Id: I74361b4d29b76fe556b493096d416836fe037819

--- releasetools.py
+++ releasetools.py
@@ -14,6 +14,6 @@
 
 def FullOTA_InstallEnd(info):
   info.script.Mount("/system")
-  info.script.AppendExtra('assert(run_program("/system/etc/init.asus.cam.ditlib.sh") == 0);')
+  info.script.AppendExtra('assert(run_program("/sbin/sh", "/system/etc/init.asus.cam.ditlib.sh") == 0);')
   info.script.AppendExtra('delete("/system/etc/init.asus.cam.ditlib.sh");')
   info.script.Unmount("/system")
