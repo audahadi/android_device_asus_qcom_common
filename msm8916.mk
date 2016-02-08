eef9540814741dd774f1b971358acfb078a5fec2
Luca Stefani <luca.stefani.ge1@gmail.com>
2016-02-08 15:30:12 +0100

    msm8916-common: Move dalvik heap to device

--- msm8916.mk
+++ msm8916.mk
@@ -45,7 +45,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
 
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
 
 # Audio
