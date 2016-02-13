e0934388fef1abc1ecc0bcd4f7ccb55ea167264c
Roman Birg <roman@cyngn.com>
2016-02-13 09:14:13 -0800

    msm8916-common: Implement old keyhandler interface
    Change-Id: I43e4d30c8b19ff6934bdbc4755639761903f4581
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- keyhandler/src/com/cyanogenmod/settings/device/KeyHandler.java
+++ keyhandler/src/com/cyanogenmod/settings/device/KeyHandler.java
@@ -274,6 +274,12 @@ public class KeyHandler implements DeviceKeyHandler {
         }
     }
 
+    @Override
+    public boolean handleKeyEvent(KeyEvent event) {
+        return handleKeyEvent(event, null);
+    }
+
+    @Override
     public boolean handleKeyEvent(KeyEvent event, DeviceHandlerCallback callback) {
         boolean isKeySupported = ArrayUtils.contains(sSupportedGestures, event.getScanCode());
         if (isKeySupported && !mEventHandler.hasMessages(GESTURE_REQUEST)) {
