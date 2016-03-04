c518ac9081dcd6766769dfb56e5db2f935433626
Luca Stefani <luca.stefani.ge1@gmail.com>
2016-03-04 06:17:29 -0800

    msm8916-common: Lower default vibration intensity
    Change-Id: I28f5c8d95d5f390935c6b253fa3f641453fa3eb6

--- cmhw/org/cyanogenmod/hardware/VibratorHW.java
+++ cmhw/org/cyanogenmod/hardware/VibratorHW.java
@@ -42,7 +42,7 @@ public class VibratorHW {
         return Integer.parseInt(FileUtils.readOneLine(AMP_PATH));
     }
     public static int getDefaultIntensity()  {
-        return 27;
+        return 20;
     }
     public static boolean setIntensity(int intensity)  {
         return FileUtils.writeLine(AMP_PATH, String.valueOf(intensity));
