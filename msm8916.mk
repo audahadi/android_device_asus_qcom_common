af2fc3458f5f6d69bb6561e57f56b2dd2f0afb08
Luca Stefani <luca.stefani.ge1@gmail.com>
2016-04-14 04:27:56 -0700

    msm8916-common: Remove msm_irqbalance config
    * Leftover from the service remove
    
    Change-Id: I33cba05c1c16a3353edd526345ce2c60233cfb5f

--- msm8916.mk
+++ msm8916.mk
@@ -117,10 +117,6 @@ PRODUCT_PACKAGES += \
     izat.conf \
     sap.conf
 
-# IRQ balance
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/msm_irqbalance.conf:system/vendor/etc/msm_irqbalance.conf
-
 # IRSC
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
