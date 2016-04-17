03e193700df1602b319b5b90d1cf954087b55c24
LuK1337 <priv.luk@gmail.com>
2016-04-17 18:39:52 +0200

    msm8916-common: Remove tags & ramdisk offsets
    * They aren't being used.
    
    Change-Id: I99a394f37cd5127d4f34d3089fb3a269a417d638

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -53,8 +53,6 @@ BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
-BOARD_KERNEL_TAGS_OFFSET := 0x00000100
-BOARD_RAMDISK_OFFSET := 0x02000000
 TARGET_KERNEL_SOURCE := kernel/asus/msm8916
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
