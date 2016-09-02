b09d632041a2f5fc6da01bcd980a6828ead126b3
Luca Stefani <luca.stefani.ge1@gmail.com>
2016-09-02 18:14:04 +0200

    msm8916-common: Import CAF audio configs
    * CAF Package version: LA.BR.1.2.9-00810-8x16.0
    
    Change-Id: I2a1d0795d21d31e2a39b4b64ed64bd199314e290

--- msm8916.mk
+++ msm8916.mk
@@ -72,6 +72,15 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths_mtp_ZE600KL.xml:system/etc/mixer_paths_mtp_ZE600KL.xml \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml
 
+# XML Audio configuration files
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:/system/etc/a2dp_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:/system/etc/audio_policy_volumes.xml \
+    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:/system/etc/default_volume_tables.xml \
+    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:/system/etc/r_submix_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:/system/etc/usb_audio_policy_configuration.xml
+
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService \
