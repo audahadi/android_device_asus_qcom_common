5d66abe42dbe480457e6d303612bf69261775222
Michael Gernoth <michael@gernoth.net>
2016-09-16 17:03:23 +0200

    msm8916-common: ril: Return static radio capability from getRadioCapability
    * Our modem somehow returns incorrect radio
      capabilities for 2nd slot, that makes it
      stay on EDGE.
    
    Change-Id: I7b490d39304a03aac55c86c44a5af86c6a6dea72

--- msm8916.mk
+++ msm8916.mk
@@ -75,6 +75,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:/system/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:/system/etc/usb_audio_policy_configuration.xml
 
+PRODUCT_PACKAGES += \
+    Snap \
+    Gello
+
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService \
