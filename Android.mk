6d2aeb1a3491bb2ef74cf6ab867d86f5660d8394
audahadi <wan.audahadi@gmail.com>
2016-10-06 01:40:07 +0800

    add zc550kl (asus_z010d)
    Change-Id: I832c858f126881db284d644352a9502b44d46199

--- Android.mk
+++ Android.mk
@@ -15,7 +15,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter Z00T Z00L,$(TARGET_DEVICE)),)
+ifneq ($(filter Z00T Z00L Z010D,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
