a5ed138aea40826f5829d6b6227e6f455f5c273d
Hashbang173 <hashbang173@gmail.com>
2016-10-05 18:06:09 -0400

    msm8916-common: Use common GPU drivers
    Change-Id: I7815a41b465f6a158425475d34810d5ddc2515f1

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -39,6 +39,8 @@ write_headers "Z00L Z00T"
 # The standard common blobs
 write_makefiles "$MY_DIR"/proprietary-files.txt
 
+printf '\n%s\n' "\$(call inherit-product, vendor/qcom/binaries/msm8916/graphics/graphics-vendor.mk)" >> "$PRODUCTMK"
+
 # We are done!
 write_footers
 
