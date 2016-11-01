b08b10225c381c464b29d2193fc52be4ee2d1ed2
LuK1337 <priv.luk@gmail.com>
2016-11-01 14:05:35 +0100

    msm8916-common: Add and enable IMS
    This reverts commit 480d1446bc2689dde029bf387c6464e4f19cbbdc.
    
    Change-Id: If8366764f97b3ea3d32c82d0bb41b3088d0a85cc

--- Android.mk
+++ Android.mk
@@ -39,4 +39,15 @@ $(shell mkdir -p $(TARGET_OUT)/etc/firmware/wlan/prima; \
     ln -sf /data/misc/wifi/WCNSS_qcom_cfg.ini \
 	    $(TARGET_OUT)/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini)
 
+IMS_LIBS := libimscamera_jni.so libimsmedia_jni.so
+
+IMS_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR_APPS)/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
+$(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "IMS lib link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /system/vendor/lib64/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(IMS_SYMLINKS)
+
 endif
