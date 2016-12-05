41d53c4b56ae97b9192b81e01f7514c0614f7661
Luca Stefani <luca.stefani.ge1@gmail.com>
2016-12-05 17:25:22 +0100

    msm8916-common: Remove keyhandler
    Change-Id: I7e918b97b01769e1e40f735bc8094068c712148c

--- msm8916.mk
+++ msm8916.mk
@@ -141,10 +141,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/synaptics_dsx.kl:system/usr/keylayout/synaptics_dsx.kl \
     $(LOCAL_PATH)/keylayout/synaptics_rmi4_i2c.kl:system/usr/keylayout/synaptics_rmi4_i2c.kl
 
-# Keyhandler
-PRODUCT_PACKAGES += \
-    com.cyanogenmod.keyhandler
-
 # Keystore
 PRODUCT_PACKAGES += \
     keystore.msm8916
