c59a8397b3ebd032b2ea255d82955c4134e455d6
Luca Stefani <luca.stefani.ge1@gmail.com>
2016-12-05 17:25:22 +0100

    msm8916-common: Remove keyhandler
    Change-Id: I7e918b97b01769e1e40f735bc8094068c712148c
    
    Conflicts:
    	msm8916.mk

--- msm8916.mk
+++ msm8916.mk
@@ -149,10 +149,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
 
-# Keyhandler
-PRODUCT_PACKAGES += \
-    com.cyanogenmod.keyhandler
-
 #PRODUCT_PACKAGES += \
 #    keystore.msm8916
 
