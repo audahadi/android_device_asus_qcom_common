479fe7c36d22e58dcbd8aaa586aab0ade48ded42
audahadi <wan.audahadi@gmail.com>
2016-12-09 20:39:44 +0800

    aosp fixes..snatch from PureNexus
    https://github.com/PureNexusProject/android_vendor_pure/commit/2356f0dc7a1eff2c05b6737ff4c8a1004e4c4bea

--- base.mk
+++ base.mk
@@ -890,3 +890,14 @@ ifeq ($(TARGET_BUILD_VARIANT),user)
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES+= \
     ro.adb.secure=1
 endif
+
+# Google property overides
+PRODUCT_PROPERTY_OVERRIDES += \
+    keyguard.no_require_sim=true \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+    ro.com.google.clientidbase=android-google \
+    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.error.receiver.system.apps=com.google.android.gms \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.com.android.dataroaming=false
