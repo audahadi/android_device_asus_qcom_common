c6979907f736bc27ebb83bfdd335e58ff683a638
audahadi <wan.audahadi@gmail.com>
2017-08-20 00:26:43 +0800

    releasetool - inject Z010DD kernel and firmware

--- msm8916.mk
+++ msm8916.mk
@@ -219,7 +219,8 @@ PRODUCT_PACKAGES += \
     IMSEnabler
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/thermal/thermal-engine-8916.conf:system/etc/thermal-engine-8916.conf
+    $(LOCAL_PATH)/thermal/thermal-engine-8916.conf:system/etc/thermal-engine-8916.conf \
+    $(LOCAL_PATH)/thermal/thermal-engine-8939.conf:system/etc/thermal-engine-8939.conf
 
 # Voice recognition
 PRODUCT_COPY_FILES += \
