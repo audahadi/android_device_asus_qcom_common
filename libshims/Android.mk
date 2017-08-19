ba7cbe938351c8a859d6cd0d35ba51f142279a2c
Luca Stefani <luca020400@lineageos.org>
2017-08-19 12:10:27 +0200

    msm8916-common: gps: Add get_process_name shim
    Change-Id: I10ce654b14866d41610c7e4098df9691b983d515

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -59,13 +59,10 @@ include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := wvm.c
+LOCAL_SRC_FILES := \
+    get_process_name/get_process_name.c
 
-LOCAL_SHARED_LIBRARIES := libstagefright_foundation
-
-LOCAL_MODULE := libshims_wvm
+LOCAL_MODULE := libshims_get_process_name
 LOCAL_MODULE_TAGS := optional
-
-LOCAL_32_BIT_ONLY := true
 
 include $(BUILD_SHARED_LIBRARY)
