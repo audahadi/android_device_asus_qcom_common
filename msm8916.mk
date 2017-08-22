38e4453c57dff4c0c9e9b83cf37dadd6ab935647
Rashed Abdel-Tawab <rashed@linux.com>
2017-08-22 14:01:07 -0700

    msm8916-common: Move Lineage specific overlays to overlay-lineage
    Change-Id: Ic38bb1acb4d37f90b62679e4420815f49b30fc7f

--- msm8916.mk
+++ msm8916.mk
@@ -15,7 +15,9 @@
 #
 
 # Overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay/common
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Permissions
 PRODUCT_COPY_FILES += \
