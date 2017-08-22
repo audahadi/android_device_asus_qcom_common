6895bfa80b4fd8a5a09ebec29b53d9bb4cd786b3
Rashed Abdel-Tawab <rashed@linux.com>
2017-08-22 14:01:07 -0700

    msm8916-common: Move Lineage specific overlays to overlay-lineage
    Change-Id: Ic38bb1acb4d37f90b62679e4420815f49b30fc7f

--- msm8916.mk
+++ msm8916.mk
@@ -18,7 +18,9 @@
 $(call inherit-product, vendor/asus/msm8916-common/msm8916-common-vendor.mk)
 
 # Overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
