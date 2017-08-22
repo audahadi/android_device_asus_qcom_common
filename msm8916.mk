b4bc8bf41211c3dd0c76b231411621b02851b995
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:17:09 +0200

    msm8916-common: Build Vibrator HAL

--- msm8916.mk
+++ msm8916.mk
@@ -250,6 +250,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/thermal/thermal-engine-8939-ze600kl.conf:system/etc/thermal-engine-8939-ze600kl.conf \
     $(LOCAL_PATH)/thermal/thermal-engine-8939-ze601kl.conf:system/etc/thermal-engine-8939-ze601kl.conf
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl
+
 # Voice recognition
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
