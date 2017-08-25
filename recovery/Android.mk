c53ef9b8ada126047f414d0e47d25b44c2ab0a0a
LuK1337 <priv.luk@gmail.com>
2017-08-25 13:37:00 +0200

    msm8916-common: recovery: Update for O
    Change-Id: I1f01837e18acd6971c0aaf410db87a54ad34afbc

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -1,8 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_C_INCLUDES := bootable/recovery \
-    bootable/recovery/updater/include
+LOCAL_C_INCLUDES := $(call project-path-for,recovery) \
+    $(call project-path-for,recovery)/updater/include
 LOCAL_SRC_FILES := recovery_updater.cpp
 LOCAL_MODULE := librecovery_updater_asus
 LOCAL_MODULE_TAGS := eng
