27c740116024c39b1d25cb8fc7c8a4868b933fad
LuK1337 <priv.luk@gmail.com>
2017-09-05 19:53:26 +0200

    msm8916-common: Build our MultiHAL sensors HAL

--- msm8916.mk
+++ msm8916.mk
@@ -270,8 +270,12 @@ PRODUCT_PACKAGES += \
     IMSEnabler
 
 # Sensors
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/_hals.conf:system/vendor/etc/sensors/_hals.conf
+
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl
+    android.hardware.sensors@1.0-impl \
+    sensors.msm8916
 
 # Thermal
 PRODUCT_COPY_FILES += \
