3e8a54f0aa9f9ecb1275127fc74c4d9583431a9a
Ethan Chen <intervigil@gmail.com>
2017-09-29 23:14:23 -0700

    msm8916-common: Remove init.qcom.bt.sh
    * Just run the hci_qcomm_init binary instead
    
    Change-Id: If2b4b9fc738e729bd389f3f96e6843a4ca6eb558

--- msm8916.mk
+++ msm8916.mk
@@ -215,7 +215,6 @@ PRODUCT_PACKAGES += \
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \
-    init.qcom.bt.sh \
     init.qcom.power.sh \
     init.qcom.factory.sh
 
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -11,13 +11,6 @@ include $(BUILD_PREBUILT)
 
 # Configuration scripts
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.bt.sh
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.qcom.bt.sh
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.power.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
