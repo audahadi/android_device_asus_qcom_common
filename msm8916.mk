ef8b2d7e14d9862aaac6d1f1be984b1e483489e8
LuK1337 <priv.luk@gmail.com>
2017-12-20 01:06:37 +0100

    msm8916-common: Convert audio_effects.conf to XML
    * Converted using : https://github.com/luk1337/aeffects-conf2xml

--- msm8916.mk
+++ msm8916.mk
@@ -71,7 +71,7 @@ PRODUCT_PACKAGES += \
 # Audio configuration
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt \
-    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    $(LOCAL_PATH)/audio/audio_effects.xml:system/vendor/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/mixer_paths_mtp.xml:system/etc/mixer_paths_mtp.xml \
     $(LOCAL_PATH)/audio/mixer_paths_mtp_ZD551KL.xml:system/etc/mixer_paths_mtp_ZD551KL.xml \
     $(LOCAL_PATH)/audio/mixer_paths_mtp_ZE600KL.xml:system/etc/mixer_paths_mtp_ZE600KL.xml \
