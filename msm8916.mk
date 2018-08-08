c57de70a85e762c6ebe776b87086fc437273f108
audahadi <wan.audahadi@gmail.com>
2018-08-09 00:41:20 +0800

    use prebuilt keystore

--- msm8916.mk
+++ msm8916.mk
@@ -195,8 +195,8 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-service
 
 # Keystore
-PRODUCT_PACKAGES += \
-    keystore.msm8916
+#PRODUCT_PACKAGES += \
+#    keystore.msm8916
 
 # Keymaster HAL
 PRODUCT_PACKAGES += \
